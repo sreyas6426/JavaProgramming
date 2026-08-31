f0f9898d585a5f3cc4a9fc01af17f35dbb13747f
sreyas6426 <sreyas6426@gmail.com>
2026-08-31 11:38:01 +0530

    RevStr

---
+++ RevStr.java
@@ -0,0 +1,10 @@
+public class RevStr 
+{
+    public static void main(String[] args) 
+    {
+        char a[] = {'h','e','l','l','o'};
+
+        for(int i = a.length - 1; i >= 0; i--)
+            System.out.print(a[i]);
+    }
+}
