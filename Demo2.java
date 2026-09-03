ed87d721fba1087a5040b605cec07bda737dbda9
sreyas6426 <sreyas6426@gmail.com>
2026-09-03 11:33:23 +0530

    Demo2

---
+++ Demo2.java
@@ -0,0 +1,9 @@
+class Demo2 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Java");
+        System.out.println("Programming");
+        System.out.println("Class");
+    }
+}
